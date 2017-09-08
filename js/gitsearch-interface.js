abc6dda1b211aff0bfc91dbb83dfc4c7435a4739
toelapiut <toelapiut7@gmail.com>
2017-09-08 15:44:13 +0300

    added code to help collect user input

--- js/gitsearch-interface.js
+++ js/gitsearch-interface.js
@@ -1,1 +1,10 @@
 var UserProfile=require('./../js/gitsearch.js').UserProfileModule;
+ var userInput;
+$(document).ready(function() {
+  $('#search').on('keyup', function(event) {
+    console.log(event.target.value);
+    userInput = event.target.value;
+    var userGit = new UserProfile();
+    userGit.getProfile(userInput);
+    console.log(userInput);
+  });
