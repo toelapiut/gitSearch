b44b088ff703aa2d1a21bc3c0bf7bcc2a62def2a
toelapiut <toelapiut7@gmail.com>
2017-09-08 15:58:21 +0300

    added theuser creditials

--- js/gitsearch.js
+++ js/gitsearch.js
@@ -57,6 +57,9 @@ UserProfile.prototype.getProfile = function(userInput) {
                        <div class="well">
                          <ul class="list-group">
                            <li class="list-group-item"><h5>Name:</h5>${response.name}</li>
+                           <li class="list-group-item"><h5>Email:</h5> ${response.email}</li>
+             <li class="list-group-item"><h5>Location:</h5> ${response.location}</li>
+             <li class="list-group-item"><h5>Hireable:</h5> ${response.hireable}</li>
                  </div>
                 </div>
            `)
